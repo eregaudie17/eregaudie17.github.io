c09667239234b9350e6379a23645b1ee4be6f043
Emmanuel Regaudie <emmanuelregaudie5@gmail.com>
2025-12-15 17:45:17 +0100

    Conf

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,6 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
-import { resolve } from "path";
-
 export default defineConfig({
   plugins: [tailwindcss()],
-  base: "/https://regaudie.netlify.app/",
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        projet1: resolve(__dirname, "pages/projet1.html"),
-      },
-    },
-  },
 });
